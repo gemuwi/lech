89bea6dc9fa793e4ab6493edd455695fe6f74527
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-08-29 07:48:06 +0530

    Update __init__.py

--- lazyleech/__init__.py
+++ lazyleech/__init__.py
@@ -1,19 +1,3 @@
-# lazyleech - Telegram bot primarily to leech from torrents and upload to Telegram
-# Copyright (c) 2021 lazyleech developers <theblankx protonmail com, meliodas_bot protonmail com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import os
 import logging
 import aiohttp
@@ -21,14 +5,14 @@ from io import BytesIO, StringIO
 from pyrogram import Client
 
 API_ID = os.environ.get('API_ID', 4665778)
-API_HASH = os.environ.get('10e3ed833b0d09699973420d45359409')
-BOT_TOKEN = os.environ.get('5613702848:AAHqmEv47v3PHuJBkMbO_AYMb-AUC4cq8js')
+API_HASH = os.environ.get('API_HASH', "10e3ed833b0d09699973420d45359409")
+BOT_TOKEN = os.environ.get('BOT_TOKEN', "5613702848:AAHqmEv47v3PHuJBkMbO_AYMb-AUC4cq8js")
 TESTMODE = os.environ.get('TESTMODE')
 TESTMODE = TESTMODE and TESTMODE != '0'
 
-EVERYONE_CHATS = os.environ.get('-1001762606230')
+EVERYONE_CHATS = os.environ.get('EVERYONE_CHATS', "-1001762606230")
 EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else []
-ADMIN_CHATS = os.environ.get('-1001762606230')
+ADMIN_CHATS = os.environ.get('ADMIN_CHATS', "-1001762606230")
 ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else []
 ALL_CHATS = EVERYONE_CHATS + ADMIN_CHATS
 
